99f28e6611a9a5f5a36932435df085eed027be9b
Callahan, Michael Joseph <callahm3@miamioh.edu>
2015-08-06 14:50:17 -0400

    Added Puppetfile for using latest apt version. Using new apt features

---
+++ Puppetfile
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+#^syntax detection
+
+# use dependencies defined in metadata.json
+metadata
+
+# Modules we have patched and are waiting on a merge
+mod 'puppetlabs-apt',
+    :git => 'https://github.com/puppetlabs/puppetlabs-apt.git',
+    :ref => 'master'
--- manifests/package/apt.pp
+++ manifests/package/apt.pp
@@ -13,10 +13,10 @@ class httpproxy::package::apt {
   if $httpproxy::ensure == present { $ensure = file }
   else { $ensure = absent }
   # Sets the proxy using the apt module with settings defined in init.pp. Then includes parent class.
-  apt::setting { 'conf-proxy':
-    ensure   => $ensure,
-    priority => '01',
-    content  => "Acquire::http::Proxy \"${httpproxy::proxy_uri}\";",
-  }
-
+  class { 'apt':
+    proxy {
+      ensure => $ensure
+      host => $httpproxy::http_proxy,
+      port => $httpproxy::http_proxy_port,
+    }
 }
