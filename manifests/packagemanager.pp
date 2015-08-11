c683e5104549f9c16db195d06f1262c269347d64
Callahan, Michael Joseph <callahm3@miamioh.edu>
2015-08-11 09:37:09 -0400

    Fixed #7

--- manifests/packagemanager.pp
+++ manifests/packagemanager.pp
@@ -4,11 +4,11 @@ class httpproxy::packagemanager {
 
   case $::osfamily {
     'RedHat': {
-      contain '::httpproxy::package::yum'
+      contain ::httpproxy::package::yum
     }
     'Debian': {
-      contain '::httpproxy::package::apt'
-      if $httpproxy::purge_apt_conf { contain '::httpproxy::package::purge_apt_conf' }
+      contain ::httpproxy::package::apt
+      if $httpproxy::purge_apt_conf { contain ::httpproxy::package::purge_apt_conf }
     }
     default: {fail ('your distro is not supported')}
   }
